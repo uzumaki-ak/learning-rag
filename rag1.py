2c539fdde387ab17ea03d9c0d09bb5a860c25ce3
uzumaki-ak <anikydv38@gmail.com>
2025-09-03 17:23:10 +0530

    learned to store in the faiss

--- rag1.py
+++ rag1.py
@@ -1,5 +1,6 @@
 import requests
 import numpy as np
+import faiss
 
 with open("unstructured-data.txt", "r", encoding="utf-8") as file:
     d1 = file.read()
@@ -22,7 +23,7 @@ print(len(chunks[1]))
 # !Embedding model
 
 
-GEMINI_API_KEY = "your api key here"
+GEMINI_API_KEY = ""
 
 def get_embedding(text, model="models/embedding-001"):
     url = f"https://generativelanguage.googleapis.com/v1beta/{model}:embedContent?key={GEMINI_API_KEY}"
@@ -63,3 +64,22 @@ print(len(test_embedding))
 
 print("First 10 values:", test_embedding[:10])
 print("Embedding vector:", test_embedding)
+
+# ! Create FAISS index and add embeddings
+dimension = test_embedding.shape[0]
+# to store all info in faiss we willl creare inddex for test-emb to store in faiss 
+index = faiss.IndexFlatL2(dimension)
+chunk_mapping = []
+
+# noqw wht we have to do i to convert entire chunk into their respective embeddings and store into the faiss index 
+# tis will  give all the chunks one buy one 
+for  chunk in chunks:
+    # now call gett emb func and try to pass all thec hunks one by one 
+    emb = get_embedding(chunk)
+    print(emb)
+    # now we have to add this emb to the faiss index and when adding we have to convert it into numpy array 
+    index.add(np.array([emb]).astype('float32'))
+# now we have to maintain the mapping of chunk to their respective embeddings append to the chunk mapping list 
+    chunk_mapping.append(chunk)
+    # now storing the db in the loal if not did this its stored in the ram 
+    faiss.write_index(index, "faiss_index.index")
